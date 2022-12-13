fc33f75acd4b1db4f79e6e46a1bfc540a103d0b9
adheeshsu <112802199+adheeshsu@users.noreply.github.com>
2022-12-13 20:11:30 +0530

    Create ATC_rotation_positionpath.c
    The above code is about the rotation direction of the ATC magazine and the shortest past from actual position to demand position.

---
+++ ATC_rotation_positionpath.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{   int n,i,A,B,C;
+    printf("enter the maximum tools in the magazine: ");
+    scanf("%d",&n);
+    int max_tools[n];
+    int actual_pos,demand_pos;
+    
+    for(i=1;i<=n;i++)
+    {
+        max_tools[i] = i;
+    }
+    printf("print the actual position of tool: ");
+    scanf("%d",&actual_pos);
+    printf("print the demanded position of tool: ");
+    scanf("%d",&demand_pos);
+    
+    A = abs(max_tools[actual_pos] - max_tools[demand_pos]);
+    B = max_tools[n] + max_tools[actual_pos]  - max_tools[demand_pos];
+    C = max_tools[n] - max_tools[actual_pos]  + max_tools[demand_pos];
+    
+    if(A < B && A < C)
+    {
+        if (demand_pos < actual_pos)
+        {
+            printf("rotate anticlock\n");
+            printf("the number of positions for the shortest path is %d",A);
+        }
+        else
+          {
+             printf("rotate clockwise\n");
+             printf("the number of positions for the shortest path is %d",A);
+          }
+    }
+    
+    else if (B < C)
+          {
+            printf("rotate anticlock\n");
+            printf("the number of positions for the shortest path is %d",B);
+          }
+    else
+          {
+            printf("rotate clockwise\n");
+            printf("the number of positions for the shortest path is %d",C);
+          }   
+}
